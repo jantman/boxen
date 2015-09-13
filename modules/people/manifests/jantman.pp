8990378f56b37b0436a22c8ec4c6885b59c8987c
Jason Antman <jason@jasonantman.com>
2015-09-13 11:15:11 -0400

    move libreoffice to my personal manifest

--- modules/people/manifests/jantman.pp
+++ modules/people/manifests/jantman.pp
@@ -8,13 +8,15 @@ class people::jantman {
   include emacs
   include wget
 
-  #class { 'libreoffice':
-  #  version => '5.0.1',
-  #}
-
-  #class { 'libreoffice::languagepack':
-  #  version => '5.0.1',
-  #  locale  => 'us',
+  # boxen/puppetlabs-libreoffice and all its forks are broken; do this here
+  $libreoffice_version = '5.0.1'
+  package { "LibreOffice-${libreoffice_version}":
+    provider => 'appdmg',
+    source   => "http://download.documentfoundation.org/libreoffice/stable/${libreoffice_version}/mac/x86_64/LibreOffice_${libreoffice_version}_MacOS_x86-64.dmg",
+  }
+  #package { 'LibreOffice-LanguagePack':
+  #  provider => 'appdmg',
+  #  source   => "http://download.documentfoundation.org/libreoffice/stable/${libreoffice_version}/mac/x86_64/LibreOffice_${libreoffice_version}_MacOS_x86-64_langpack_${locale}.dmg",
   #}
 
   class {'vagrant':
